0cb334ced30cb2ca892c380781f6de7cd06ffd23
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-06-03 23:54:20 +0100

    import packages in ingestion files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,10 @@
 # data ingestion is apart f model by reading data from source like database or API
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging 
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+import dataclasses import dataclasses
