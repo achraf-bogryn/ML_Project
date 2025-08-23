cf8875d9eab9a8c11e88ab97b0b81ccb88918d01
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-08-23 20:13:20 +0100

    call model trainer inside data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer , ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -57,5 +58,8 @@ if __name__ == "__main__":
     logging.info("Data ingestion completed successfully.")
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr , test_arr , _ =data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
             
