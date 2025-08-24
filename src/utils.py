fe2a57a2bde5249762b19af7ba51d8f6b74f44b0
achraf-bogryn <achrafbogryn12345@gmail.com>
2025-08-24 20:15:33 +0100

    add new methode in utils folder

--- src/utils.py
+++ src/utils.py
@@ -61,3 +61,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models , param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+         try:
+                with open(file_path,'rb') as file_obj:
+                     return dill.load(file_obj)
+         except Exception as e:
+             logging.info('Exception Occured in load_object function utils')
+             raise CustomException(e,sys)       
+        
+
+
+     
